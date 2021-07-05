85dca49ba33a8f449ab52356142422f726d6e350
Rhys Holloway <rhyswilliamholloway@gmail.com>
2021-07-05 15:41:24 -0700

    first commit

---
+++ src/party.rs
@@ -0,0 +1,25 @@
+use pokedex::{
+    gui::{party::PartyGui, pokemon::PokemonDisplay},
+    battle::party::BattleParty,
+    pokemon::instance::PokemonInstance,
+};
+
+pub fn battle_party_gui<
+    ID: Sized + Copy + core::fmt::Debug + core::fmt::Display + Eq + Ord,
+    A,
+>(
+    gui: &PartyGui,
+    party: &BattleParty<ID, A, PokemonInstance>,
+    exitable: bool,
+) {
+    gui.spawn(
+        party
+            .pokemon
+            .iter()
+            .cloned()
+            .map(|instance| PokemonDisplay::new(std::borrow::Cow::Owned(instance)))
+            .collect(),
+        Some(false),
+        exitable,
+    );
+}
